717360d20934334cfbb6eed0b5b8192c633364a0
Margo Astreyko <margoastreyko@mail.ru>
2020-09-10 02:14:15 +0300

    Updates

---
+++ precache-manifest.0a0b8521424652b25105d0d75cf8ca1e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba02db089c452f885095016b312dd841",
+    "url": "/my-store/index.html"
+  },
+  {
+    "revision": "567726d7318314001b1a",
+    "url": "/my-store/static/css/main.8c3eb537.chunk.css"
+  },
+  {
+    "revision": "0c14f06e3e38c4873a6b",
+    "url": "/my-store/static/js/2.562c270e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/my-store/static/js/2.562c270e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "567726d7318314001b1a",
+    "url": "/my-store/static/js/main.19a3490b.chunk.js"
+  },
+  {
+    "revision": "987ef40f0fe544093fd0",
+    "url": "/my-store/static/js/runtime-main.f8e212d0.js"
+  }
+]);
